2ce421cee216bbddaad3cb06d6619f7ad321b71c
Leon Munene <leonmunene@gmail.com>
2025-05-26 15:54:10 +0300

    udt

--- learn/next.config.mjs
+++ learn/next.config.mjs
@@ -2,6 +2,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    runtime: 'experimental-edge'
+  }
   
 };
 if (process.env.NODE_ENV === 'development') {
