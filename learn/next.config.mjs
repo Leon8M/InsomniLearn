c0c3342f17aac6447adc533591d2a674baff06b9
Leon Munene <leonmunene@gmail.com>
2025-05-30 10:54:03 +0300

    eslint2

--- learn/next.config.mjs
+++ learn/next.config.mjs
@@ -2,10 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    runtime: 'experimental-edge'
-  }
-  
 };
 if (process.env.NODE_ENV === 'development') {
    await setupDevPlatform();
